3704af99c5411563d1ff3a56ed03e2459084cfb8
nostalgic-math-student <josue.rojas@ciencias.unam.mx>
2023-12-09 23:02:07 -0600

    frontend implementation - Upload and cypher

--- frontend/src/components/Cypher.jsx
+++ frontend/src/components/Cypher.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { saveAs } from 'file-saver';
+import axios from 'axios';
 
 const Cypher = () => {
 
-  const handleFileInput = (e) => {
-    // Access the selected file
-    const file = e.target.files[0];
+  const [file, setFile] = useState(null);
+  const [password, setPassword] = useState('');
+  const [outputName, setOutputName] = useState('');
+  const [totalEvs, setTotalEvs] = useState(0);
+  const [minPoints, setMinPoints] = useState(0);
 
-    // Log the file name
-    if (file) {
-        console.log("Selected file: ", file.name);
-    }
-};
+  const handleFileInput = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleOutputNameChange = (e) => {
+    setOutputName(e.target.value);
+  };
+
+  const handleTotalEvsChange = (e) => {
+    setTotalEvs(e.target.value);
+  };
+
+  const handleMinPointsChange = (e) => {
+    setMinPoints(e.target.value);
+  };
 
 const postFile = async (fileName) => {
+  
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('password', password);
+  formData.append('OutputName', outputName);
+  formData.append('total_evs', totalEvs);
+  formData.append('min_points', minPoints);
+
   try {
-    const response = await fetch('http://localhost:3001/api/cypher', {
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({fileName: fileName})
+    const response = await axios.post('http://127.0.0.1:5000/encrypt', formData, {
+      responseType: 'blob', // Importante para manejar la respuesta como un blob
     });
-    const data = await response.json();
-    console.log("response:", data)
+
+    const blob = new Blob([response.data], {type: 'application/octet-stream'})
+    saveAs(blob, outputName+".enc")
   }
   catch(error){
     console.error("Error Posting:", error)
@@ -31,14 +54,17 @@ const postFile = async (fileName) => {
 
   return (
     <div>
-      <h1>
-      Cypher
-      </h1>
-
-    <div className='bg-color-100'>
-      <input type="file" onChange={handleFileInput}/>
-    </div>
-
+      <h1>Cypher</h1>
+      <div className='bg-color-100'>
+        <input type="file" onChange={handleFileInput} />
+        <input type="text" placeholder="Password" onChange={handlePasswordChange} />
+        <input type="text" placeholder="Output Name" onChange={handleOutputNameChange} />
+        <input type="number" placeholder="Total EVs" onChange={handleTotalEvsChange} />
+        <input type="number" placeholder="Min Points" onChange={handleMinPointsChange} />
+        <button className='btn btn-primary' onClick={postFile}>
+          Send
+        </button>
+      </div>
     </div>
   )
 }
